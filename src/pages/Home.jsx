b39a2dca7797a7bd5939dfc5ebcc234a0390e6d0
Daniel Palmares <danielpalmares100@gmail.com>
2021-04-27 21:15:15 -0300

    styles: new find recipes card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { IoGridOutline } from 'react-icons/io5';
+import { IoGridOutline, IoArrowForward } from 'react-icons/io5';
 
 import Avatars from '../assets/avatars/index';
+import StarsIcon from '../assets/svg/brilhante.svg';
+
 import styles from '../styles/pages/home.module.scss';
 
 export default function Home() {
@@ -16,6 +18,22 @@ export default function Home() {
           <img src={Avatars.ladyAvatar1} alt="User Profile" />
         </div>
       </header>
+
+      <main>
+        <div className={styles.findRecipesCard}>
+          <div className={styles.iconContainer}>
+            <img src={StarsIcon} alt="Stars Icon" />
+          </div>
+
+          <div className={styles.textContainer}>
+            <p>Find recipes based on what you already have at home</p>
+            <button>
+              Let's try!
+              <IoArrowForward size={16} />
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
