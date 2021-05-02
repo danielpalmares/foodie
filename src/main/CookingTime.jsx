5f60464f1bfc82087248e17eca701870de07c494
Daniel Palmares <danielpalmares100@gmail.com>
2021-05-02 03:49:52 +0000

    new: axios, api, private keys

--- src/main/CookingTime.jsx
+++ src/main/CookingTime.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_SPOONACULAR_API_KEY;
 
 export default class CookingTime extends Component {
   constructor(props) {
