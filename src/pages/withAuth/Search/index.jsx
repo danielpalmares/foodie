ded772be3238651a66f4df06417b971b77254461
Daniel Palmares <danielpalmares100@gmail.com>
2021-06-03 22:19:30 +0000

    fix: re-render pagination at last point or fetching new recipes

--- src/pages/withAuth/Search/index.jsx
+++ src/pages/withAuth/Search/index.jsx
@@ -22,15 +22,16 @@ export default function Search() {
   const dispatch = useDispatch();
   const recipesRef = useRef();
 
-  // get all recipes from state
+  // access to all recipes from state
   const recipes = useSelector(
     state => state.recipesByIngredientsReducer.data?.recipes
   );
 
   // states for pagination
   const [recipesPerPage, setRecipesPerPage] = useState([]);
-  const [resultsPerPage, setResultsPerPage] = useState(10);
+  const [resultsPerPage, setResultsPerPage] = useState(10); // magic number
   const [currentPage, setCurrentPage] = useState(1);
+  const [maxPages, setMaxPages] = useState(null);
   const [previousButton, setPreviousButton] = useState(false);
   const [nextButton, setNextButton] = useState(false);
   // states for other functionalities
@@ -45,6 +46,9 @@ export default function Search() {
 
     return dispatch(recipesByIngredientsAction(inputIngredients));
   }, [inputIngredients]);
+
+  // set spinner to false whenever the recipes has loaded
+  useEffect(() => setSpinner(false), [recipes]);
 
   // get the recipes per page and store it in the state
   useEffect(() => {
@@ -66,9 +70,6 @@ export default function Search() {
     return pagination(recipes, currentPage, resultsPerPage);
   }, [recipesPerPage, currentPage]);
 
-  // set spinner to not show up whenever the recipes has loaded
-  useEffect(() => setSpinner(false), [recipes]);
-
   // get the recipes for each page
   function getSearchResultsPage(recipesArr, curPage, resultsPerPage) {
     const start = (curPage - 1) * resultsPerPage;
@@ -77,46 +78,13 @@ export default function Search() {
     return recipesArr.slice(start, end);
   }
 
-  // handle pagination button click event
-  function handlePagination(prevOrNext) {
-    // set current page
-    prevOrNext === 'previous'
-      ? setCurrentPage(currentPage - 1)
-      : setCurrentPage(currentPage + 1);
-
-    // scroll to the top of the recipes
-    recipesRef.current.scrollIntoView({ behavior: 'smooth' });
-    return;
-  }
-
-  // setting up the inputIngredients when the user searches
-  function handleSearchRecipes() {
-    // back to default
-    currentPage !== 1 && setCurrentPage(1);
-    nextButton && setNextButton(false);
-    previousButton && setPreviousButton(false);
-
-    const ingredients = formatInputString(inputData);
-    setInputIngredients(ingredients);
-    return;
-  }
-
-  // formatting input data
-  function formatInputString(inputString) {
-    const ingredientsString = inputString;
-    const ingredientsArr = ingredientsString.replace(/\s+/g, ' ').split(',');
-    const ingredientsArrFormatted = ingredientsArr.map(ing =>
-      ing.trim().replace(' ', '+').toLowerCase()
-    );
-    const ingredientsFormatted = ingredientsArrFormatted.join(',');
-    return ingredientsFormatted;
-  }
-
+  // pagination functionality
   function pagination(recipes, currentPage, resultsPerPage) {
     if (!recipes) return;
 
     // get the number of pages
     const numPages = Math.ceil(recipes.length / resultsPerPage); // array full of recipes / resultsPerPage = 20
+    setMaxPages(numPages);
 
     // possible situations
     // 1) page one, and there are other pages
@@ -141,7 +109,59 @@ export default function Search() {
     }
 
     // 4) page one, and there are no other pages
+    setPreviousButton(false);
+    setNextButton(false);
     return;
+  }
+
+  // handle pagination click event
+  function handlePagination(prevOrNext) {
+    // set current page
+    prevOrNext === 'previous'
+      ? setCurrentPage(currentPage - 1)
+      : setCurrentPage(currentPage + 1);
+
+    // scroll to the top of the recipes
+    // recipesRef.current.scrollIntoView({ behavior: 'smooth' });
+    return;
+  }
+
+  // handle search click event (setting up the inputIngredients when the user searches)
+  function handleSearchRecipes() {
+    const ingredients = formatInputString(inputData);
+    // check if the ingredients are the same as before
+    if (ingredients === inputIngredients) return;
+
+    // if not, then get stuff back to default and set up ingredients to fetch after
+    backToDefault();
+    setInputIngredients(ingredients);
+
+    return;
+  }
+
+  // get stuff back to default, usually when searching for new recipes
+  function backToDefault() {
+    previousButton && setPreviousButton(false);
+    nextButton && setNextButton(false);
+    recipesPerPage.length !== 0 && setRecipesPerPage([]);
+    currentPage !== 1 && setCurrentPage(1);
+
+    return;
+  }
+
+  // format input data
+  function formatInputString(inputString) {
+    const ingredientsString = inputString;
+    // check the whole string, replace double whitespace or more for one, and then split them by comma into an array
+    const ingredientsArr = ingredientsString.replace(/\s+/g, ' ').split(',');
+    // for each ingredient inside the array, remove whitespace from start and end, for others just replace for +
+    const ingredientsArrFormatted = ingredientsArr.map(ing =>
+      ing.trim().replace(' ', '+').toLowerCase()
+    );
+    // transform them back to string separated by comma
+    const ingredientsFormatted = ingredientsArrFormatted.join(',');
+
+    return ingredientsFormatted;
   }
 
   return (
@@ -167,35 +187,32 @@ export default function Search() {
         <GridLayout ref={recipesRef}>
           {spinner && <Spinner />}
 
-          {recipesPerPage &&
-            recipesPerPage.map(rec => {
-              return (
-                <RecipeCard
-                  key={rec.id}
-                  title={rec.title}
-                  imageSrc={rec.image}
-                  likes={rec.likes}
-                />
-              );
-            })}
+          {recipesPerPage && !spinner
+            ? recipesPerPage.map(rec => {
+                return (
+                  <RecipeCard
+                    key={rec.id}
+                    title={rec.title}
+                    imageSrc={rec.image}
+                    likes={rec.likes}
+                  />
+                );
+              })
+            : ''}
         </GridLayout>
 
         <PaginationContainer>
-          {previousButton && spinner === false ? (
+          {previousButton && spinner === false && currentPage > 1 && (
             <PreviousPageButton
               handleClick={() => handlePagination('previous')}
               value={currentPage - 1}
             />
-          ) : (
-            ''
           )}
-          {nextButton && spinner === false ? (
+          {nextButton && spinner === false && currentPage < maxPages && (
             <NextPageButton
               handleClick={() => handlePagination('next')}
               value={currentPage + 1}
             />
-          ) : (
-            ''
           )}
         </PaginationContainer>
       </SearchContainer>
